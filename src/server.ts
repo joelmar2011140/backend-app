7b3ff892da84d66c2d63178da4ea0668897892b1
Joel Marinho <joeldeveloper2011140@gmail.com>
2023-08-10 17:14:14 +0100

    feat: add code to execute a web server instance

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import { createServer } from 'http'
+import dotenv from 'dotenv'
+import { logger } from './lib/logger'
+import { app } from './app/app'
+
+dotenv.config()
+
+function startServer (): void {
+  const port = process.env.PORT ?? 4321
+  const server = createServer(app)
+  server.listen(port, async () => {
+    try {
+      logger.info(`Server running on port ${port}`)
+    } catch (err: any) {
+      logger.error('Error on startServer: ', err)
+      process.exit(1)
+    }
+  })
+}
+
+startServer()
